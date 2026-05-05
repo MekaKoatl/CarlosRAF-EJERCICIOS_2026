252f26222f4e6b54c206d33185ab7b6405226db3
MekaKoatl <carlos-raf@outlook.com>
2026-05-05 18:50:39 +0200

    Clase 05_05_26

---
+++ M2_4_Express/api/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(cors());
+
+//----------------------
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//----------------------
+
+const persona = [
+  { nombre: "Carlos", apellido: "V", edad: 26, nacionalidad: "MXN" },
+  { nombre: "Luca", apellido: "Luko", edad: 21, nacionalidad: "ESP" },
+  { nombre: "Cris", apellido: "Kros", edad: 21, nacionalidad: "ESP" },
+  { nombre: "Karla", apellido: "Max", edad: 29, nacionalidad: "RD" },
+  { nombre: "Salma", apellido: "Amlas", edad: 22, nacionalidad: "ESP" },
+];
+
+app.listen(process.env.PORT || 3000);
